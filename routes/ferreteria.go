c450d20f695fc6795b0f33155e3bee6733e79154
Chris Cromer <chris@cromer.cl>
2022-05-14 00:31:07 -0400

    cambiar ruta de lista de ferreterias a plural

--- routes/ferreteria.go
+++ routes/ferreteria.go
@@ -7,7 +7,7 @@ import (
 
 func FerreteriaRoutes(router *httprouter.Router) {
 	router.POST("/ferreteria", controllers.PostFerreteria)
-	router.GET("/ferreteria", controllers.ListFerreterias)
+	router.GET("/ferreterias", controllers.ListFerreterias)
 	router.GET("/ferreteria/:id", controllers.GetFerreteria)
 	router.PUT("/ferreteria/:id", controllers.PutFerreteria)
 	router.PATCH("/ferreteria/:id", controllers.PatchFerreteria)
