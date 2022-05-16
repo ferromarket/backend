fa0db97feaeaa5db69cf704927b3cd34d80bd95e
Chris Cromer <chris@cromer.cl>
2022-05-15 20:59:25 -0400

    crear modelo de repartidor

--- main.go
+++ main.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/ferromarket/backend/controllers"
 	"github.com/ferromarket/backend/routes"
 )
 
 func main() {
+    allArgs := os.Args[1:]
+
+    dropDB := false
+    populateDB := false
+
+    for _, element := range allArgs {
+        if (element == "drop") {
+            dropDB = true
+        }
+
+        if (element == "populate") {
+            populateDB = true
+        }
+    }
+
     fmt.Print("Connecting to database ... ")
     db := controllers.DBConnect()
     fmt.Println("DONE")
 
+    if (dropDB) {
+        fmt.Print("Dropping database ... ")
+        controllers.DBDropAll(db)
+        fmt.Println("DONE")
+    }
+
     fmt.Print("AutoMigrating database ... ")
     controllers.DBAutoMigrate(db)
     fmt.Println("DONE")
+
+    if (populateDB) {
+        fmt.Print("Populating database ... ")
+        controllers.DBPopulate(db)
+        fmt.Println("DONE")
+    }
 
     fmt.Print("Closing database ... ")
     controllers.DBClose(db)
---
+++ models/usuario.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Usuario struct {
+	gorm.Model
+	ID uint64 `json:"ID" gorm:"primaryKey"`
+	RUT string `json:"RUT" gorm:"unique;size:9;not null"`
+	Contrasena string `json:"Contrasena" gorm:"not null"`
+	Email string `json:"Email" gorm:"not null"`
+	Nombres string `json:"Nombres" gorm:"not null"`
+	ApellidoPaterno string `json:"ApellidoPaterno" gorm:"not null"`
+	ApellidoMaterno string `json:"ApellidoMaterno" gorm:"not null"`
+	Telefono uint32 `json:"Telefono"`
+	Direccion string `json:"Direccion" gorm:"not null"`
+	FechaNacimiento time.Time
+}
