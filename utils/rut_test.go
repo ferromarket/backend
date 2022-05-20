836ba9e3acb272372118b1fd297b8968620b60c6
Chris Cromer <chris@cromer.cl>
2022-05-19 22:43:10 -0400

    agregar mas pruebas de RUT

--- utils/rut_test.go
+++ utils/rut_test.go
@@ -11,3 +11,19 @@ func TestGenerateVerifier(t *testing.T) {
 		t.Fatalf(`generateVerifier("8675309") = %q, %v, want match for %#q, nil`, msg, err, want)
 	} 
 }
+
+func TestGenerateVerifier2(t *testing.T) {
+	want := "9"
+	msg, err := generateVerifier("86753095")
+	if (msg != want) {
+		t.Fatalf(`generateVerifier("8675309") = %q, %v, want match for %q, nil`, msg, err, want)
+	} 
+}
+
+func TestGenerateVerifierInvalidString(t *testing.T) {
+	want := "invalid RUT"
+	_, err := generateVerifier("8675f309")
+	if (err.Error() != want) {
+		t.Fatalf(`generateVerifier("8675f309") = %q, want %q, nil`, err.Error(), want)
+	}
+}
