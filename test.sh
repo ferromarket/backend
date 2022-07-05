9dca38c3d36397452c76cd97f0cbb3f131d5aac7
Chris Cromer <chris@cromer.cl>
2022-07-05 00:31:38 -0400

    agregar informe en formato html

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+if [ ! -f "3rd_party/go-test-report/go-test-report" ]; then
+    pushd 3rd_party/go-test-report
+    go build
+    popd
+fi
+
+go test -json ./... | 3rd_party/go-test-report/go-test-report -g 10 -s 40 -t "Ferrormarket backend tests"
+
 go test -coverprofile=coverage.out ./...
 go tool cover -html=coverage.out -o test_coverage.html
