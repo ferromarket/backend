370bfadef610162e624e51b57b49a03035b97a30
Cristianlr95 <cristia.lagos199507@gmail.com>
2022-08-02 20:52:19 -0400

    Correcion comentarios propuestos

--- models/repartidor.go
+++ models/repartidor.go
@@ -1,24 +1,23 @@
 package models
 
 import (
-	"time"
-
+	"github.com/ferromarket/backend/utils"
 	"gorm.io/gorm"
 )
 
 type Repartidor struct {
 	gorm.Model
-	ID uint64 `json:"ID" gorm:"primaryKey"`
-	RUT string `json:"RUT" gorm:"unique;size:9;not null"`
-	Contrasena string `json:"Contrasena" gorm:"not null"`
-	Email string `json:"Email" gorm:"not null"`
-	Nombres string `json:"Nombres" gorm:"not null"`
-	ApellidoPaterno string `json:"ApellidoPaterno" gorm:"not null"`
-	ApellidoMaterno string `json:"ApellidoMaterno" gorm:"not null"`
-	Telefono uint32 `json:"Telefono"`
-	Direccion string `json:"Direccion" gorm:"not null"`
-	FechaNacimiento time.Time
-	FechaRegistracion time.Time `json:"FechaRegistracion"`
-	TipoLicencia uint8 `json:"TipoLicencia"`
-	FechaLicencia time.Time `json:"FechaLicencia"`
+	ID                uint64 `json:"ID" gorm:"primaryKey"`
+	RUT               string `json:"RUT" gorm:"unique;size:9;not null"`
+	Contrasena        string `json:"Contrasena" gorm:"not null"`
+	Email             string `json:"Email" gorm:"not null"`
+	Nombres           string `json:"Nombres" gorm:"not null"`
+	ApellidoPaterno   string `json:"ApellidoPaterno" gorm:"not null"`
+	ApellidoMaterno   string `json:"ApellidoMaterno" gorm:"not null"`
+	Telefono          uint32 `json:"Telefono"`
+	Direccion         string `json:"Direccion" gorm:"not null"`
+	FechaNacimiento   utils.DateTime
+	FechaRegistracion utils.DateTime `json:"FechaRegistracion"`
+	TipoLicencia      uint8          `json:"TipoLicencia"`
+	FechaLicencia     utils.DateTime `json:"FechaLicencia"`
 }
