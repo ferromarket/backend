6343378def7488fb470e7e798667700c6285d75f
Cristianlr95 <cristia.lagos199507@gmail.com>
2022-08-02 20:52:19 -0400

    Correcion comentarios propuestos

--- controllers/repartidor_test.go
+++ controllers/repartidor_test.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"regexp"
 	"testing"
-	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/ferromarket/backend/database"
@@ -24,15 +23,15 @@ func repartidorData() (models.Repartidor, *sqlmock.Rows) {
 		ApellidoMaterno:   "Ciruela",
 		Telefono:          99834553,
 		Direccion:         "Calle falsa 123",
-		FechaNacimiento:   time.Now(),
-		FechaRegistracion: time.Now(),
+		FechaNacimiento:   utils.DateTime{},
+		FechaRegistracion: utils.DateTime{},
 		TipoLicencia:      2,
-		FechaLicencia:     time.Now(),
+		FechaLicencia:     utils.DateTime{},
 	}
 
 	rows := sqlmock.NewRows([]string{"id", "created_at", "updated_at", "deleted_at", "rut", "contrasena", "email", "nombres", "apellido_paterno", "apellido_materno", "telefono", "direccion", "fecha_nacimiento", "fecha_registracion", "tipo_licencia", "fecha_licencia"}).
-		AddRow("1", time.Time{}, time.Time{}, nil, repartidor.RUT, repartidor.Contrasena, repartidor.Email, repartidor.Nombres, repartidor.ApellidoPaterno, repartidor.ApellidoMaterno, repartidor.Telefono, repartidor.Direccion, repartidor.FechaNacimiento, repartidor.FechaRegistracion, repartidor.TipoLicencia, repartidor.FechaLicencia).
-		AddRow("2", time.Time{}, time.Time{}, nil, repartidor.RUT+"2", repartidor.Contrasena+"2", repartidor.Email+"2", repartidor.Nombres+"2", repartidor.ApellidoPaterno+"2", repartidor.ApellidoMaterno+"2", repartidor.Telefono+2, repartidor.Direccion+"2", repartidor.FechaNacimiento, repartidor.FechaRegistracion, 1, repartidor.FechaLicencia)
+		AddRow("1", utils.DateTimeNow(), utils.DateTimeNow(), nil, repartidor.RUT, repartidor.Contrasena, repartidor.Email, repartidor.Nombres, repartidor.ApellidoPaterno, repartidor.ApellidoMaterno, repartidor.Telefono, repartidor.Direccion, repartidor.FechaNacimiento, repartidor.FechaRegistracion, repartidor.TipoLicencia, repartidor.FechaLicencia).
+		AddRow("2", utils.DateTimeNow(), utils.DateTimeNow(), nil, repartidor.RUT+"2", repartidor.Contrasena+"2", repartidor.Email+"2", repartidor.Nombres+"2", repartidor.ApellidoPaterno+"2", repartidor.ApellidoMaterno+"2", repartidor.Telefono+2, repartidor.Direccion+"2", repartidor.FechaNacimiento, repartidor.FechaRegistracion, 1, repartidor.FechaLicencia)
 
 	return repartidor, rows
 }
