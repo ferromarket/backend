75a22c028163c62f6a248995d600a6ab78df7fa5
John Valdebenito Sharp <john.valdebenito1601@alumnos.ubiobio.cl>
2022-08-05 00:58:34 -0400

    Arreglé errores y consulta de listProductos

--- controllers/producto.go
+++ controllers/producto.go
@@ -15,7 +15,7 @@ type Productos struct {
 	Productos []models.Producto `json:"Producto"`
 }
 
-func PostProductos(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+func PostProducto(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
 	gdb := database.Connect()
 
 	decoder := json.NewDecoder(request.Body)
@@ -29,7 +29,7 @@ func PostProductos(writer http.ResponseWriter, request *http.Request, params htt
 		json.NewEncoder(writer).Encode(utils.ErrorMessage{ErrorMessage: err.Error()})
 	}
 
-	err = PostProducto(producto, gdb)
+	err = postProducto(producto, gdb)
 	if err != nil {
 		writer.Header().Set("Content-Type", "application/json")
 		writer.WriteHeader(http.StatusBadRequest)
@@ -40,7 +40,7 @@ func PostProductos(writer http.ResponseWriter, request *http.Request, params htt
 	database.Close(gdb)
 }
 
-func PostProducto(producto models.Producto, gdb *gorm.DB) error {
+func postProducto(producto models.Producto, gdb *gorm.DB) error {
 	return gdb.Create(&producto).Error
 }
 
@@ -64,8 +64,7 @@ func ListProductos(writer http.ResponseWriter, request *http.Request, params htt
 }
 
 func listProductos(productos *[]models.Producto, gdb *gorm.DB) *gorm.DB {
-	gdb.Model(&models.Producto{}).Order("ID asc").Preload("Nombre").Find(&productos)
-
+	return gdb.Model(&models.Producto{}).Order("ID asc").Preload("Categoria").Find(&productos)
 }
 
 func GetProducto(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
