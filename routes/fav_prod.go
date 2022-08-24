fcce3d9f0d337144ce4fe786f1102ffe1b658b4e
Javier Ignacio González Reyes <javier.gonzalez1601@alumnos.ubiobio.cl>
2022-08-23 20:51:10 -0400

    fixed PR

--- routes/fav_prod.go
+++ routes/fav_prod.go
@@ -7,10 +7,10 @@ import (
 )
 
 func FavProdRoutes(router *httprouter.Router) {
-	router.POST("/favProd", middlewares.Authenticate(controllers.PostFavProd))
-	router.GET("/favProd", middlewares.Authenticate(controllers.ListFavProd))
-	router.GET("/favProd/:id", middlewares.Authenticate(controllers.GetFavProd))
-	router.PUT("/favProd/:id", middlewares.Authenticate(controllers.PutFavProd))
-	router.PATCH("/favProd/:id", middlewares.Authenticate(controllers.PatchFavProd))
-	router.DELETE("/favProd/:id", middlewares.Authenticate(controllers.DeleteFavProd))
+	router.POST("/favprod", middlewares.Authenticate(controllers.PostFavProd))
+	router.GET("/favprod", middlewares.Authenticate(controllers.ListFavProd))
+	router.GET("/favprod/:id", middlewares.Authenticate(controllers.GetFavProd))
+	router.PUT("/favprod/:id", middlewares.Authenticate(controllers.PutFavProd))
+	router.PATCH("/favprod/:id", middlewares.Authenticate(controllers.PatchFavProd))
+	router.DELETE("/favprod/:id", middlewares.Authenticate(controllers.DeleteFavProd))
 }
