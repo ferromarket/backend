5d780e5bc948c90c199137462df83c1ff602fe48
Javier Ignacio González Reyes <javier.gonzalez1601@alumnos.ubiobio.cl>
2022-08-24 01:44:40 -0400

    update usuario registrado

---
+++ models/usuario_registrado_test.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestUsuarioEmptyName(t *testing.T) {
+	usuario := Usuario{Nombres: ""}
+	want := "nombre vacío"
+	err := usuario.Validate()
+	if err == nil || err.Error() != want {
+		t.Fatalf(`usuario.Validate() = %v, want match for %#q, nil`, err, want)
+	}
+}
+
+func TestUsuarioNotEmptyName(t *testing.T) {
+	usuario := Usuario{Nombres: "juan Heriberto", Direccion: "Algo"}
+	var want error = nil
+	err := usuario.Validate()
+	if err != nil {
+		t.Fatalf(`usuario.Validate() = %v, want match for %#q, nil`, err, want)
+	}
+}
