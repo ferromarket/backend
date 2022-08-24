4a202c27fb24a1bbb5b4f116c9f4c2ad4cd5cec9
Javier Ignacio González Reyes <javier.gonzalez1601@alumnos.ubiobio.cl>
2022-08-23 20:32:49 -0400

    fixed PR

--- models/fav_prod.go
+++ models/fav_prod.go
@@ -7,8 +7,8 @@ import (
 type FavProd struct {
 	gorm.Model
 	ID         uint64  `json:"ID" gorm:"primaryKey"`
-	ProdID     uint64  `json:"IDProd" gorm:"not null"`
-	UsuarioID  uint64  `json:"IDUsuario" gorm:"not null"`
+	ProdID     uint64  `json:"ProdID" gorm:"not null"`
+	UsuarioID  uint64  `json:"UsuarioID" gorm:"not null"`
 	Usuario    Usuario `json:"Usuario" gorm:"not null"`
 	NombreProd string  `json:"NombreProd" gorm:"not null"`
 }
