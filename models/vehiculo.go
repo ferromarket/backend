5085e75eddbc7aa724b63a33108419be2e45d952
Cristianlr95 <cristia.lagos199507@gmail.com>
2022-09-08 00:38:03 -0300

    cambios a vehiculo

--- models/vehiculo.go
+++ models/vehiculo.go
@@ -8,12 +8,12 @@ import (
 type Vehiculo struct {
 	gorm.Model
 	ID                 uint64     `json:"ID" gorm:"primaryKey"`
-	RepartidorID       uint64     `json:"RepartidorID" gorm:"unique;not null"`
-	Repartidor         Repartidor `json:"Repartidor" gorm:"not null"`
-	Patente            string     `json:"Patente" gorm:"unique;not null"`
-	Marca              string     `json:"Marca" gorm:"not null"`
-	Modelo             string     `json:"Modelo" gorm:"not null"`
-	Ano                uint16     `json:"Ano" gorm:"not null"`
-	PermisoCirculacion utils.Date `json:"PermisoCirculacion" gorm:"not null"`
-	Seguro             utils.Date `json:"Seguro" gorm:"not null"`
+	RepartidorID       uint64     `json:"RepartidorID"`
+	Repartidor         Repartidor `json:"Repartidor"`
+	Patente            string     `json:"Patente"`
+	Marca              string     `json:"Marca"`
+	Modelo             string     `json:"Modelo"`
+	Ano                uint16     `json:"Ano"`
+	PermisoCirculacion utils.Date `json:"PermisoCirculacion"`
+	Seguro             utils.Date `json:"Seguro"`
 }
